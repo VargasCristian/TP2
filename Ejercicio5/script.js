2d1ae9b25dacf7d941c0520f6c6089f7107ac9e4
Cristian Vargas <cristianvargas122@outlook.com>
2024-05-30 18:38:24 -0300

    Realización del ejercicio 5

---
+++ Ejercicio5/script.js
@@ -0,0 +1,16 @@
+// script.js
+document.getElementById("clickMe").onclick = test;
+
+function test() {
+    var ladoA = parseFloat(document.getElementById("ladoA").value);
+    var ladoB = parseFloat(document.getElementById("ladoB").value);
+    var ladoC = parseFloat(document.getElementById("ladoC").value);
+
+    if (ladoA <= 0 || ladoB <= 0 || ladoC <= 0) {
+        alert("Error: Las dimensiones deben ser números positivos.");
+        return;
+    }
+
+    var area = (((ladoA - ladoC) * ladoB / 2) + (ladoB * ladoC));
+    alert("El área es de " + area.toFixed(2) + " metros cuadrados");
+}
